5fd7def1ba1a42e3444bcf1f69760313e1f1f8a9
Leonardo Hubner <leonardo_hubner@hotmail.com>
2023-05-01 16:35:14 -0300

    Implementando exceções e personalizando algumas mensagens

---
+++ logistiks-api/src/main/java/com/hubner/logistiksapi/api/exceptionhandler/Problem.java
@@ -0,0 +1,24 @@
+package com.hubner.logistiksapi.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Problem {
+	private Integer status;
+	private LocalDateTime dateTime;
+	private String title;
+	private List<Field> campos;
+	
+	@AllArgsConstructor
+	@Getter
+	public static class Field{
+		private String fieldName;
+		private String message;
+	}
+}
